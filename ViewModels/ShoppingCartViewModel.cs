5c815e80bc05ef63b7105b739593b735b45d4015
Konstantin Pankratov <konstantin.pankratov@live.com>
2024-03-09 19:51:11 -0500

    Added Partial Views

---
+++ ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,17 @@
+﻿using BethanysPieShop.Models;
+
+namespace BethanysPieShop.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public IShoppingCart ShoppingCart { get; }
+        public decimal ShoppingCartTotal {  get; }
+
+        public ShoppingCartViewModel(IShoppingCart shopingCart, decimal shoppingCartTotal)
+        {
+            ShoppingCart = shopingCart;
+            ShoppingCartTotal = shoppingCartTotal; 
+        }
+
+    }
+}
